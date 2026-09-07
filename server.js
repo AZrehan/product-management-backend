23daa7af67816b554ff46dd8bd348a99d3d7be25
AZrehan <rehanarbaaz57@gmail.com>
2026-09-07 11:16:49 +0530

    url

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const productRoutes = require("./src/routes/product.routes")
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["https://vercel.com/arbaaz-rehans-projects/product-management-frontend/5TmpxnwAhj9NKvih7Wzx5TjzwtYZ",
+        "http://localhost:5173"],
     credentials: true
 }))
 connectDB();
