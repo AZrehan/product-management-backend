1e2b1f2a934e63cb22232c8b905ee80b2d830871
AZrehan <rehanarbaaz57@gmail.com>
2026-09-07 11:27:28 +0530

    url3

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const productRoutes = require("./src/routes/product.routes")
 
 const app = express()
 app.use(cors({
-    origin: ["https://product-management-frontend-kjqn8dcgz-arbaaz-rehans-projects.vercel.app/",
+    origin: ["https://product-management-frontend-lac.vercel.app",
         "http://localhost:5173"],
     credentials: true
 }))
